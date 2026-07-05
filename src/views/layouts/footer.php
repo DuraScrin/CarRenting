eaf66c7911a31fea114f2edb7eb7fe780918672d
dura.software <dura.software@outlook.com>
2026-07-05 12:16:01 +0200

    Remove redundant root src/ folder, public_html/src is the single source

--- src/views/layouts/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="site-footer">
-    <div class="container footer-inner">
-        <p>&copy; <?php echo date('Y'); ?> GreenDrive Car Rental. All rights reserved.</p>
-    </div>
-</footer>
